21a76430eb26d9f7df829b0291373e3e72fb435c
Sahil Baori <96388615+SBaori@users.noreply.github.com>
2024-06-15 22:39:35 +0530

    Update quine.hs

--- quine.hs
+++ quine.hs
@@ -1,5 +1,5 @@
 src :: String
-src = "src :: String\nsrc = \"?\"\n\ngetSrc :: String\ngetSrc = construct src \n    where\n        construct :: String -> String\n        construct \"\" = \"\" \n        construct (c:cs)\n            | fromEnum c == 63 = helper src ++ construct cs\n            | otherwise = c:construct cs \n            where\n                helper \"\" = \"\"\n                helper (c:cs)\n                    | c == '\\n' = '\\\\':'n':helper cs\n                    | c == '\\\\' = '\\\\':'\\\\':helper cs\n                    | c == '\"' = '\\\\':'\"':helper cs\n                    | otherwise = c:helper cs\n\nmain :: IO ()\nmain = putStr getSrc"
+src = "src :: String\nsrc = \"?\"\n\ngetSrc :: String\ngetSrc = construct src \n    where\n        construct :: String -> String\n        construct \"\" = \"\" \n        construct (c:cs)\n            | fromEnum c == 63 = helper src ++ construct cs\n            | otherwise = c:construct cs \n            where\n                helper :: String -> String\n                helper \"\" = \"\"\n                helper (c:cs)\n                    | c == '\\n' = '\\\\':'n':helper cs\n                    | c == '\\\\' = '\\\\':'\\\\':helper cs\n                    | c == '\"' = '\\\\':'\"':helper cs\n                    | otherwise = c:helper cs\n\nmain :: IO ()\nmain = putStr getSrc"
 
 getSrc :: String
 getSrc = construct src 
@@ -10,6 +10,7 @@ getSrc = construct src
             | fromEnum c == 63 = helper src ++ construct cs
             | otherwise = c:construct cs 
             where
+                helper :: String -> String
                 helper "" = ""
                 helper (c:cs)
                     | c == '\n' = '\\':'n':helper cs
