fd544d40272f8fa39d68858397cdcbdd083c823d
Sahil Baori <96388615+SBaori@users.noreply.github.com>
2024-06-16 01:08:10 +0530

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,4 @@
+src=$(cat src.hs)
+blob=$(echo -n "$src" | sed 's/\\/\\\\\\\\/g; s/"/\\\\"/g' | sed ':a;N;$!ba;s/\n/\\\\n/g')
+echo -n "$src" | sed "s/\"?\"/\"$blob\"/g" > quine.hs
+echo "Generated quine.hs"
