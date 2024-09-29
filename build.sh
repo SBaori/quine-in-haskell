c6b10eb17d1bb03807f2ec2114d9ddb0d8c57cc3
Sahil Baori <96388615+SBaori@users.noreply.github.com>
2024-09-29 20:54:53 +0530

    Delete build.sh

--- build.sh
+++
@@ -1,4 +0,0 @@
-src=$(cat src.hs)
-blob=$(echo -n "$src" | sed 's/\\/\\\\\\\\/g; s/"/\\\\"/g' | sed ':a;N;$!ba;s/\n/\\\\n/g')
-echo -n "$src" | sed "s/?/$blob/g" > quine.hs
-echo "Generated quine.hs"
